6777aff0a356f13c897712c93eff0718d6eddc2c
Michael Sprauer <Sprauer@Inline.de>
2015-03-04 12:02:02 +0100

    send the name of the engine and the model in the form:load event

--- app/views/smarter_listing/_form_logic.js
+++
@@ -1,9 +0,0 @@
-var name = 'form:load';
-var event;
-
-if (typeof Prototype !== 'undefined') {
-    Event.fire(document, name, void 0, true);
-}
-event = document.createEvent('Events');
-event.initEvent(name, true, true);
-document.dispatchEvent(event);
